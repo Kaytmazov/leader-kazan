edaa602e8b73e9cfc24789c2d64d61e9d7794e6f
Shamil Kaytmazov <altair@list.ru>
2017-11-20 20:14:03 +0300

    Add scheme, calculate, contacts, footer and modal

--- js/script.js
+++ js/script.js
@@ -2,4 +2,12 @@ $('.modal-img').on('click', function(e) {
   e.preventDefault();
   $('.imagepreview').attr('src', $(this).find('img').attr('src'));
   $('#imagemodal').modal('show');
-});     
+});
+
+// In your Javascript (external .js resource or <script> tag)
+$(document).ready(function() {
+    $('.select2').select2({
+      placeholder: 'Не выбрано',
+      minimumResultsForSearch: Infinity
+    });
+});
