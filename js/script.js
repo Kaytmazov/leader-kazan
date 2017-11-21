24c6188d00ba408a776d227a8b6e00b7303e63c7
Shamil Kaytmazov <altair@list.ru>
2017-11-21 20:24:44 +0300

    Styling range input

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Открывать лицензии в модальном окне
 $('.modal-img').on('click', function(e) {
   e.preventDefault();
   $('.imagepreview').attr('src', $(this).find('img').attr('src'));
@@ -10,4 +11,40 @@ $(document).ready(function() {
       placeholder: 'Не выбрано',
       minimumResultsForSearch: Infinity
     });
+});
+
+// Ползунок 
+var slider = document.getElementById('rangeQ');
+
+noUiSlider.create(slider, {
+  start: [ 15 ],
+  step: 1,
+  connect: [true, false],
+  range: {
+    'min': 0,
+    'max': 160
+  },
+  pips: {
+    mode: 'values',
+    values: [0, 10, 20, 40, 80, 120, 160],
+    density: 9999
+  },
+  format: {
+    to: function ( value ) {
+      return value;
+    },
+    from: function ( value ) {
+      return value.replace();
+    }
+  }
+});
+
+var inputFormat = document.getElementById('input-format');
+
+slider.noUiSlider.on('update', function( values, handle ) {
+  inputFormat.value = values[handle];
+});
+
+inputFormat.addEventListener('change', function(){
+  slider.noUiSlider.set(this.value);
 });
