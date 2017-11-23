0bea3c09c9758fbcf8b13f263c463b1884ea7546
Shamil Kaytmazov <altair@list.ru>
2017-11-23 19:46:01 +0300

    Add adaptive

--- js/script.js
+++ js/script.js
@@ -48,3 +48,36 @@ slider.noUiSlider.on('update', function( values, handle ) {
 inputFormat.addEventListener('change', function(){
   slider.noUiSlider.set(this.value);
 });
+
+
+// Карта
+ymaps.ready(init);
+var myMap, 
+  myPlacemark;
+
+function init(){ 
+  myMap = new ymaps.Map("map", {
+    center: [55.796663, 49.060203],
+    zoom: 16,
+    controls: []
+  }); 
+
+  myPlacemark = new ymaps.Placemark([55.796663, 49.060203], {
+    hintContent: 'Лидер-Казань'
+  }, {
+    // Опции.
+    // Необходимо указать данный тип макета.
+    iconLayout: 'default#image',
+    // Своё изображение иконки метки.
+    iconImageHref: '../img/icons/map-marker.svg',
+    // Размеры метки.
+    iconImageSize: [41, 54],
+    // Смещение левого верхнего угла иконки относительно
+    // её "ножки" (точки привязки).
+    iconImageOffset: [-20, -53]
+  });
+  
+  myMap.geoObjects.add(myPlacemark);
+
+  myMap.behaviors.disable('scrollZoom');
+}
